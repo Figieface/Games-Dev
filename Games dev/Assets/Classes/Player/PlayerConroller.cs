bcdef921fb3fc717415afa3cacc25e04f117c4a9
Figieface <123968609+Figieface@users.noreply.github.com>
2025-02-05 16:08:07 +0000

    Test scene. Player movement. LFS.
    Test scene created: capsule for player, plane for map.
    
    Player movement: Input map created, Player controller to take inputs, WASD movement working.
    
    Git LFS installed.

---
+++ Games dev/Assets/Classes/Player/PlayerConroller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerConroller : MonoBehaviour
+{
+    private Vector2 _input;
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _characterController.Move(_direction * speed * Time.deltaTime);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+        Debug.Log(_input);
+    }
+
+}
